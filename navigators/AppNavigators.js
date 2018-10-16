c14cd504f1291b65ac91f3ba5de9676c62901135
v_qianmaoyin <562510392@qq.com>
2018-10-16 17:07:18 +0800

    通过配置`navigationOptions`来去掉头部导航

--- navigators/AppNavigators.js
+++ navigators/AppNavigators.js
@@ -10,12 +10,19 @@ import Page2 from '../pages/Page2'
 //创建一个常量AppStackNavigation
 export const AppStackNavigation = StackNavigator({
     HomePage: {
-        screen: HomePage
+        screen: HomePage,
+        // navigationOptions:{//去掉头部导航，写在这里只影响HomePage页面
+        //     header:null
+        // }
     },
     Page1:{
         screen:Page1
     },
     Page2:{
         screen:Page2
+    }
+},{
+    navigationOptions:{//去掉头部导航，写在这里是全局的，影响所有的页面
+        header:null
     }
 });
